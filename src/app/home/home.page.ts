89100ed0146ff5ed00dede4d8db84b244ca71594
Gabriel <gab.serrano@duocuc.cl>
2023-12-11 05:01:58 -0300

    produccion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ViewChild, ElementRef} from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonicModule, RouterModule, QRCodeModule ],
 })
 export class HomePage {
-  constructor() {}
+  qrCodeValue: string = 'https://www.dropbox.com/scl/fi/ump4gyb1j6aym4l06mj93/DuocTAP.apk?rlkey=aijfjtmx1oewpk7os7m9lcbyk&dl=0';
+  isDesktop: boolean = false;
+
+  constructor(private platform: Platform) {
+    this.isDesktop = !this.platform.is('mobile');
+  }
 }
